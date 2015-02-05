fb25ab88234ea450aafb57800bc866ed01ebd02d
blakeaholics <blakeaholics@gmail.com>
2015-02-05 00:25:26 -0800

    Slight tweaks, contacts updated.

--- app/src/main/java/org/manbearpig/mangocoinz/ContactsFragment.java
+++ app/src/main/java/org/manbearpig/mangocoinz/ContactsFragment.java
@@ -1,21 +1,29 @@
 package org.manbearpig.mangocoinz;
 
 import android.app.Fragment;
+import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import static org.manbearpig.mangocoinz.R.layout.item_user;
 
 public class ContactsFragment extends Fragment {
 	
 	public ContactsFragment(){}
-	
-	@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
- 
-        View rootView = inflater.inflate(R.layout.fragment_contacts, container, false);
-        setHasOptionsMenu(false);
-        return rootView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Preparing user interface
+        final View v = inflater.inflate(R.layout.fragment_contacts, container, false);
+        final ListView listView = (ListView) v.findViewById(R.id.lstContacts);
+        //listView.setOnItemClickListener(this);
+
+        return v;
     }
+
 }
